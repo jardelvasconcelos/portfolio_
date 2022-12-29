9ad8a6307aae5acf36f2ed277345cdc1931da00b
jardelvasconcelos <62824207+jardelvasconcelos@users.noreply.github.com>
2022-12-29 01:48:06 -0300

    Projetos - alteracao do estilo do botao de links

--- src/components/Projetos/cardProject.js
+++ src/components/Projetos/cardProject.js
@@ -1,11 +1,11 @@
 
 const Card = (props) =>
     <div className="projects_container color-6">
-        <img className="image_projects" src={props.icon} />
+        <img className= "image_projects" alt = '' src={props.icon} />
         <p className="coment_projects">{props.titulo}</p>
         <div className="button_card">
-            <a href={props.gitHub} target='_blank' className="link_card">GitHub</a>
-            <a href={props.deploy} target='_blank' className="link_card">Deploy</a>
+            <a href={props.gitHub} target='blank' className="link_card">GitHub</a>
+            <a href={props.deploy} target='blank' className="link_card">Deploy</a>
         </div>
     </div>
 export default Card;
