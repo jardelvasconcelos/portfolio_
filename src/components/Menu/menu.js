ae52eaccfe63f6fe89a2deecf87c41318627817d
Jardel <tecjardelv@gmail.com>
2023-02-16 15:39:03 -0300

    testes_conexao

--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -5,7 +5,7 @@ export default function Menu() {
         <header id="home">
             <nav className="menu">
                 <a href="#sobre" className="topcis">Sobre</a>
-                <a href="#certificados" className="topcis">Cursos</a>
+                <a href="#certificados" className="topcis">-Cursos</a>
                 <a href="#projects" className="topcis">Projetos</a>
                 <a href="#contatos" className="topcis">Contatos</a>
             </nav>
