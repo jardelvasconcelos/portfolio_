aa9f8cfdd38ab5f45982896bc353d2b3504b1169
Jardel <tecjardelv@gmail.com>
2023-03-01 10:54:54 -0300

    teste de conexao com o repositorio remoto

--- src/components/Capa/capa.js
+++ src/components/Capa/capa.js
@@ -6,7 +6,7 @@ export default function Capa (){
             <div className="content_home_content_ ">
                 <h1 className="content_title color-4">Desenvolvedor Front-End Junior</h1>
                 <p className="content_description color-4">Convido você a conhecer um pouco sobre mim e dos meus projetos.
-                    Seja muito bem-vindo(a) e espero que goste do que lhe será apresentado.</p>
+                    Seja muito bem-vindo(a), espero que goste do que lhe será apresentado.</p>
             </div>
         </section>
     )
