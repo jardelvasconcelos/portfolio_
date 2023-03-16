9d4aa45cc7373ade15ea5886a8e674dcdad35652
Jardel <tecjardelv@gmail.com>
2023-03-16 16:31:09 -0300

    inicio de incremento da funcionalidade do botão de menu

--- src/components/Buttom_menu/menu_buttom.js
+++ src/components/Buttom_menu/menu_buttom.js
@@ -1,10 +1,14 @@
 import React from "react";
+import React,{useState} from "react";
 import "./menu_buttom.css";
 
 
 export default function Buttom_Menu(){
+
+    const [openMenu , setOpenMenu] = useState();
+
     return (
-        <buttom className = "container_buttom">
+        <buttom onClick = {openMenu} className = "container_buttom">
             <a href="">
                 <div className="menu_burger"></div>
                 <div className="menu_burger"></div>
