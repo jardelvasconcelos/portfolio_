38c3ea30eca660ac253b8359a402b900cdf64dac
Jardel <tecjardelv@gmail.com>
2023-06-15 16:24:59 -0300

    inicio reformulacao na secao sobre

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer id="contatos" className="container_footer">
             <div className="footer-container-item">
-                <p className="footer-credit">Jardel Vasconcelos</p>
+                <p className="footer-credit"> &#174; Jardel Vasconcelos</p>
             </div>
         </footer >
     )
--- src/components/Sobre/sobre.js
+++ src/components/Sobre/sobre.js
@@ -3,7 +3,7 @@ import './sobre.css';
 export default function Sobre(){
     return(
         <section className=" tela_sobre section_sobre color-5" id="sobre">
-            <div className="content_home_content_">
+            <div className="content_home_content_about">
                 <h1 className="content_title font4">Um pouco sobre mim ...</h1>
                 <p className="content_description font4 ">Técnico em Eletrônica, formado em
                     Desenvolvimento de Sistemas e atualmente cursando Engenharia de Software, sou uma pessoa sedenta por
@@ -13,9 +13,9 @@ export default function Sobre(){
                     compartilhar conhecimentos com as pessoas em minha volta.
                 </p>
             </div>
-            <div className="person">
+           {/*  <div className="person">
                 <img className="image_perfil_ " src="assets/img/home/imageperfil.png" alt=""/>
-            </div>
+            </div> */}
         </section>
     )
 }
