982d249c70d2b894a465c83e613f19dac48cbe06
Jardel <tecjardelv@gmail.com>
2023-06-19 10:19:30 -0300

    insercao de imagens de projetos

--- src/components/Projetos/projetos.js
+++ src/components/Projetos/projetos.js
@@ -8,25 +8,25 @@ export default function Projetos() {
          <h1>Alguns Projetos</h1>
          <div className="my-projects">
             <Card
-               icon="assets/img/cursos_formacao/logo_react.png" alt=""
+               icon="assets/img/projetos/logo.png" alt=""
                titulo='Clinica Médica'
                gitHub='https://github.com/jardelvasconcelos/projeto_clinica_medica.git'
                deploy='https://jardelvasconcelos.github.io/projeto_clinica_medica/index.html'
             />
             <Card
-               icon="assets/img/cursos_formacao/logo_react.png" alt=""
+               icon="assets/img/projetos/manual_.png" alt=""
                titulo='Manual Técnico'
                gitHub='https://github.com/jardelvasconcelos/consult_.git'
                deploy='https://jardelvasconcelos.github.io/consult_/'
             />
             <Card
-               icon="assets/img/cursos_formacao/logo_react.png" alt=""
+               icon="assets/img/projetos/fundo_preto.jpg" alt=""
                titulo='LandPage Systech'
                gitHub='https://github.com/jardelvasconcelos/systech.git'
                deploy='https://systech-eight.vercel.app/'
             />
             <Card
-               icon="assets/img/cursos_formacao/logo_react.png" alt=""
+               icon="assets/img/projetos/sorveteria.png" alt=""
                titulo='Sorveteria'
                gitHub='https://github.com/jardelvasconcelos/sorveteria.git'
                deploy='https://sorveteria-pi.vercel.app/'
